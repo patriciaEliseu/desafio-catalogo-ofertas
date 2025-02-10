6a67bfe434e9e56de161d196d86eaf68464de4fa
Patricia Eliseu <patyeliseu4@gmail.com>
2025-02-10 15:58:27 -0300

    add models e views e urls

--- products/models.py
+++ products/models.py
@@ -3,4 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Products(models.Model):
-    
+    nome = models.CharField(max_length=255)
+    preco = models.FloatField()
+    parcelamento = models.TextField()
+    imagem = models.URLField()
+    link = models.URLField(unique=True)
+    preco_original = models.FloatField(null=True, blank=True)
+    percentual_desconto = models.FloatField(null=True, blank=True)
+    frete_gratis = models.BooleanField(default=False)
+    tipo_entrega = models.CharField(max_length=20, choices=[
+        ('Normal', 'NORMAL'),
+        ('full', 'FULL')
+    ])
+        
+    def __str__(self):
+            return f'{self.nome}'
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from models import Products
 
-# Create your views here.
+def list_products(request):
+    produtos = Products.objects.all()
+
+    if request.GET.get('frete_gratis'):
+        produtos = produtos.filter(frete_gratis=True)
+    if request.GET.get('full'):
+        produtos = produtos.filter(entrega='Full')
+    return render (request, 'list_products.html', {'produtos': produtos})
